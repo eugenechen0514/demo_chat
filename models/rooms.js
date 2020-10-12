16072454c04b5e870d8175e21c5c1e5844f67819
EugeneChen <yujiechen0514@gmail.com>
2020-10-12 21:58:00 +0800

    init rooms model

--- models/rooms.js
+++ models/rooms.js
@@ -1,7 +1,17 @@
 /**
+ * @interface RoomMessage
+ * @property {string} from user id
+ * @property {string} to user id
+ * @property {string} content
+ * @property {Date} date
+ */
+
+/**
  * @interface Room
  * @property {string} id
+ * @property {RoomMessage[]} messages
  */
+
 
 /**
  *
@@ -24,12 +34,59 @@ module.exports = {
     /**
      *
      * @param {User} user
-     * @return {Room[]}
+     * @return {Promise<Room[]>}
      */
-    getUserRooms(user) {
+    async getUserRooms(user) {
         return rooms.filter(room => {
             const ids = room.id.split('-');
             return ids.find(id => id === user.id);
         });
+    },
+
+    /**
+     *
+     * @param userA
+     * @param userB
+     * @return {Promise<Room>}
+     */
+    async ensureRoom(userA, userB) {
+        const roomId = computeRoomId(userA, userB);
+        const found = rooms.find(room => room.id === roomId);
+        if(found) {
+            return found;
+        }
+
+        /**
+         *
+         * @type {Room}
+         */
+        const room = {
+            id: roomId,
+            messages: [],
+        };
+        rooms.push(room);
+        return room;
+    },
+
+    /**
+     *
+     * @param {User} fromUser
+     * @param {User} toUser
+     * @param {string} content
+     */
+    async sendMessage(fromUser, toUser, content) {
+        /**
+         *
+         * @type {RoomMessage}
+         */
+        const message = {
+            from: fromUser.id,
+            to: toUser,
+            content,
+            date: new Date()
+        };
+
+        const room  = await ensureRoom(fromUser, toUser);
+        room.messages.push(message);
     }
 }
